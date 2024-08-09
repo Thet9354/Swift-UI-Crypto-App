0022d10f748c33645eaf8bf72c5819fc298cd40a
PhoonThetPine <thetpine254@gmail.com>
2024-08-09 13:29:19 +0800

    Initial Commit

---
+++ SwiftUICryptoApp/SwiftUICryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoAppApp.swift
+//  SwiftUICryptoApp
+//
+//  Created by Phoon Thet Pine on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
